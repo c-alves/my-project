497e47ec0731da7f87aa2ee64a5768229beb2d50
c-alves <c.alves@prodaso.de>
2022-01-30 00:32:41 +0100

    Works

---
+++ schema.js
@@ -0,0 +1,11 @@
+const schema = [{
+  "name": "Time",
+  "type": "date",
+  "format": "%b-%Y"
+}, {
+  "name": "Interest Rate",
+  "type": "number"
+}, {
+  "name": "Inflation Rate",
+  "type": "number"
+}]
